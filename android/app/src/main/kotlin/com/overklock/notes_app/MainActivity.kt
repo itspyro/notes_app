b891e2dc86efae714a02ad7259e4e0b0a88cc313
itspyro <65843269+itspyro@users.noreply.github.com>
2020-12-26 12:06:27 +0530

    application setup

---
+++ android/app/src/main/kotlin/com/overklock/notes_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.overklock.notes_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MainPage());
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  bool _initialized = false;
+  bool _error = false;
+
+  void initializeFlutterFire() async{
+    try {
+      await
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
